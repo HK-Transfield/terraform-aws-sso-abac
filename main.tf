908b0c0f5ad6447e3d91a40745adc04cf1931df5
HK Transfield <hktransfield.dev@gmail.com>
2024-08-16 15:45:08 +1200

    fix: add sso instance id

--- main.tf
+++ main.tf
@@ -9,37 +9,36 @@
 data "aws_ssoadmin_instances" "this" {} # need to grab the sso instance data
 
 locals {
-  aws_ssoadmin_instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  sso_instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  sso_instance_id  = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
 }
 
 ################################################################################
 # Attributes for Access Control
 ################################################################################
 
-resource "aws_ssoadmin_instance_access_control_attributes" "this" {
-  count = length(keys(var.attributes)) > 0 ? 1 : 0
-
-  instance_arn = local.aws_ssoadmin_instance_arn
-
-  dynamic "attribute" {
-    for_each = var.attributes
-
-    content {
-      key = attribute.key
-
-      value {
-        source = [attribute.value]
-      }
-    }
-  }
-}
+# resource "aws_ssoadmin_instance_access_control_attributes" "example" {
+#   instance_arn = local.sso_instance_arn
+#   attribute {
+#     key = "CotCenter"
+#     value {
+#       source = ["$${path:enterprise.cotCenter}"]
+#     }
+#   }
+#   attribute {
+#     key = "Oganization"
+#     value {
+#       source = ["$${path:enterprise.oganization}"]
+#     }
+#   }
+# }
 
 ################################################################################
 # Policies and Permission Sets
 ################################################################################
 
 data "aws_ssoadmin_permission_set" "this" {
-  instance_arn = local.aws_ssoadmin_instance_arn
+  instance_arn = local.sso_instance_arn
   name         = var.policy_name
   depends_on   = [aws_ssoadmin_permission_set.this]
 }
@@ -47,13 +46,13 @@ data "aws_ssoadmin_permission_set" "this" {
 resource "aws_ssoadmin_permission_set" "this" {
   name             = var.policy_name
   description      = var.policy_desc != "" ? var.policy_desc : var.policy_name
-  instance_arn     = local.aws_ssoadmin_instance_arn
+  instance_arn     = local.sso_instance_arn
   session_duration = var.session_duration
   tags             = var.tags
 }
 
 resource "aws_ssoadmin_permission_set_inline_policy" "this" {
-  instance_arn       = local.aws_ssoadmin_instance_arn
+  instance_arn       = local.sso_instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.this.arn
   inline_policy      = var.policy_json
 }
@@ -63,7 +62,7 @@ resource "aws_ssoadmin_permission_set_inline_policy" "this" {
 ################################################################################
 
 data "aws_identitystore_group" "this" {
-  identity_store_id = local.aws_ssoadmin_instance_arn
+  identity_store_id = local.sso_instance_id
 
   alternate_identifier {
     unique_attribute {
@@ -76,7 +75,7 @@ data "aws_identitystore_group" "this" {
 resource "aws_ssoadmin_account_assignment" "this" {
   principal_id       = data.aws_identitystore_group.this.id
   permission_set_arn = data.aws_ssoadmin_permission_set.this.arn
-  instance_arn       = local.aws_ssoadmin_instance_arn
+  instance_arn       = local.sso_instance_arn
   principal_type     = var.principal_type
   target_id          = var.aws_account_identifier
   target_type        = "AWS_ACCOUNT" # Keep entity type for which the assignment will be created as is
