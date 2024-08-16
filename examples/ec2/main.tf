3ecc56afcf3eca363bc77c19dfe3124dd6224727
HK Transfield <hktransfield.dev@gmail.com>
2024-08-16 15:44:05 +1200

    feat: add policy for ec2 access based on attributes

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -6,134 +6,45 @@ locals {
   region = "ap-southeast-2"
 }
 
-locals {
-  peg_project = "peg" # Pegasus project
-  uni_project = "uni" # Unicorn project
-}
-
-locals {
-  eng_team = "eng" # Engineering team
-  qas_team = "qas" # Quality Assurance team
-}
-
-locals {
-  peg_cc = "987654"
-  uni_cc = "123456"
-
-  tags = {
-    access-project = local.peg_project
-    access-team    = local.eng_team
-    cost-center    = local.peg_cc
-  }
-}
-
 ################################################################################
 # ABAC Policy
 ################################################################################
 
 data "aws_iam_policy_document" "access_same_project_team" {
+
+  # Actions allowed regardless of tags
   statement {
-    sid       = "AllActionsSecretsManagerSameProjectSameTeam"
     effect    = "Allow"
-    actions   = ["secretsmanager:*"]
+    actions   = ["ec2:DescribeInstances"]
     resources = ["*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "aws:ResourceTag/access-project"
-      values   = ["$${aws:PrincipalTag/access-project}"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "aws:ResourceTag/access-team"
-      values   = ["$${aws:PrincipalTag/access-team}"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "aws:ResourceTag/cost-center"
-      values   = ["$${aws:PrincipalTag/cost-center}"]
-    }
-
-    condition {
-      test     = "ForAllValues:StringEquals"
-      variable = "aws:TagKeys"
-
-      values = [
-        "access-project",
-        "access-team",
-        "cost-center",
-        "Name",
-        "OwnedBy",
-      ]
-    }
-
-    condition {
-      test     = "StringEqualsIfExists"
-      variable = "aws:RequestTag/access-project"
-      values   = ["$${aws:PrincipalTag/access-project}"]
-    }
-
-    condition {
-      test     = "StringEqualsIfExists"
-      variable = "aws:RequestTag/access-team"
-      values   = ["$${aws:PrincipalTag/access-team}"]
-    }
-
-    condition {
-      test     = "StringEqualsIfExists"
-      variable = "aws:RequestTag/cost-center"
-      values   = ["$${aws:PrincipalTag/cost-center}"]
-    }
   }
 
+  # Actions allowed when tags match
   statement {
-    sid       = "AllResourcesSecretsManagerNoTags"
-    effect    = "Allow"
-    resources = ["*"]
-
+    effect = "Allow"
     actions = [
-      "secretsmanager:GetRandomPassword",
-      "secretsmanager:ListSecrets",
+      "ec2:StartInstances",
+      "ec2:StopInstances"
     ]
-  }
-
-  statement {
-    sid       = "ReadSecretsManagerSameTeam"
-    effect    = "Allow"
     resources = ["*"]
-    actions = [
-      "secretsmanager:Describe*",
-      "secretsmanager:Get*",
-      "secretsmanager:List*",
-    ]
 
     condition {
       test     = "StringEquals"
-      variable = "aws:ResourceTag/access-team"
-      values   = ["$${aws:PrincipalTag/access-team}"]
+      variable = "aws:ResourceTag/CostCenter"
+      values   = ["$${aws:PrincipalTag/CostCenter}"]
     }
-  }
-
-  statement {
-    sid       = "DenyUntagSecretsManagerReservedTags"
-    effect    = "Deny"
-    resources = ["*"]
-    actions   = ["secretsmanager:UntagResource"]
 
     condition {
-      test     = "ForAnyValue:StringLike"
-      variable = "aws:TagKeys"
-      values   = ["access-*"]
+      test     = "StringEquals"
+      variable = "aws:ResourceTag/Organization"
+      values   = ["$${aws:PrincipalTag/Organization}"]
     }
-  }
 
-  statement {
-    sid       = "DenyPermissionsManagement"
-    effect    = "Deny"
-    resources = ["*"]
-    actions   = ["secretsmanager:*Policy"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:ResourceTag/Division"
+      values   = ["$${aws:PrincipalTag/Division}"]
+    }
   }
 }
 
@@ -145,7 +56,4 @@ module "sso_abac" {
   policy_name            = "Tempest-Engineers"
   policy_json            = data.aws_iam_policy_document.access_same_project_team.json
   aws_account_identifier = var.account_id
-  attributes = {
-    Organization = ""
-  }
 }
