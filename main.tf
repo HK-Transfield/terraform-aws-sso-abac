1b226c35dba03c50d41c1de3d596af805d4ed9de
HK Transfield <hktransfield.dev@gmail.com>
2024-08-16 14:19:39 +1200

    feat: add resource for access control attributes

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
-/**
-This module contains all the permission sets for a project
-It will define individual permission sets as inline policies 
-Either replication or defined as customer policies. 
-Included in that is a variable for a tag that will only 
-allow access to resources with the same tag
+################################################################################
+# title: AWS IAM Identity Center with ABAC Access
+# contributors: HK Transfield
+#
+# This module helps with configuration tasks necessary to prepare AWS 
+# resources and to set up IAM Identity Center for ABAC access.
+################################################################################
 
-Update tag for a new set of rules
-
-This module assigns AWS accounts permission sets by adding them to the appropriate
-IAM Identity Center group.
-*/
 data "aws_ssoadmin_instances" "this" {} # need to grab the sso instance data
 
 locals {
@@ -17,8 +13,36 @@ locals {
 }
 
 ################################################################################
-# ABAC Policies and Permission Sets
+# Attributes for Access Control
 ################################################################################
+
+resource "aws_ssoadmin_instance_access_control_attributes" "this" {
+  count = length(keys(var.attributes)) > 0 ? 1 : 0
+
+  instance_arn = local.aws_ssoadmin_instance_arn
+
+  dynamic "attribute" {
+    for_each = var.attributes
+
+    content {
+      key = attribute.key
+
+      value {
+        source = [attribute.value]
+      }
+    }
+  }
+}
+
+################################################################################
+# Policies and Permission Sets
+################################################################################
+
+data "aws_ssoadmin_permission_set" "this" {
+  instance_arn = local.aws_ssoadmin_instance_arn
+  name         = var.policy_name
+  depends_on   = [aws_ssoadmin_permission_set.this]
+}
 
 resource "aws_ssoadmin_permission_set" "this" {
   name             = var.policy_name
@@ -34,12 +58,6 @@ resource "aws_ssoadmin_permission_set_inline_policy" "this" {
   inline_policy      = var.policy_json
 }
 
-data "aws_ssoadmin_permission_set" "this" {
-  instance_arn = local.aws_ssoadmin_instance_arn
-  name         = var.policy_name
-  depends_on   = [aws_ssoadmin_permission_set.this]
-}
-
 ################################################################################
 # IAM Identity Center
 ################################################################################
@@ -50,15 +68,14 @@ data "aws_identitystore_group" "this" {
   alternate_identifier {
     unique_attribute {
       attribute_path  = "DisplayName"
-      attribute_value = var.group
+      attribute_value = var.policy_name
     }
   }
 }
 
 resource "aws_ssoadmin_account_assignment" "this" {
-  for_each           = var.aws_identitystore_groups # Keeping for_each as it's the only way I can think of assigning multiple Psets
-  principal_id       = each.key
-  permission_set_arn = each.value
+  principal_id       = data.aws_identitystore_group.this.id
+  permission_set_arn = data.aws_ssoadmin_permission_set.this.arn
   instance_arn       = local.aws_ssoadmin_instance_arn
   principal_type     = var.principal_type
   target_id          = var.aws_account_identifier
