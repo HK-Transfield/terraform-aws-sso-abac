7ac99591df5acbd5b9f067201d6334c876715b9f
HK Transfield <hktransfield.dev@gmail.com>
2024-08-20 16:27:44 +1200

    chore: add example account IDs

--- examples/ec2_abac/main.tf
+++ examples/ec2_abac/main.tf
@@ -2,8 +2,36 @@ provider "aws" {
   region = local.region
 }
 
+data "aws_ssoadmin_instances" "this" {}
+
 locals {
   region = "ap-southeast-2"
+}
+
+locals {
+  attributes = {
+    "CostCenter"   = "$${path:enterprise.costCenter}"
+    "Organization" = "$${path:enterprise.organization}"
+    "Division"     = "$${path:enterprise.division}"
+  }
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+resource "aws_ssoadmin_instance_access_control_attributes" "this" {
+  instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+
+  dynamic "attribute" {
+    for_each = local.attributes
+    content {
+      key = attribute.key
+      value {
+        source = [attribute.value]
+      }
+    }
+  }
 }
 
 ################################################################################
@@ -11,16 +39,15 @@ locals {
 ################################################################################
 
 locals {
-  attributes                    = ["CostCenter", "Organization", "Division"]
-  actions_allowed_no_tags       = ["ec2:DescribeInstances"]
-  actions_allowed_matching_tags = ["ec2:StartInstances", "ec2:StopInstances"]
+  actions_allowed_nonconditional = ["ec2:DescribeInstances"]
+  actions_allowed_matching_tags  = ["ec2:StartInstances", "ec2:StopInstances"]
 }
 
-module "ec2_abac" {
-  source                        = "../.."
-  policy_name                   = "Tempest-Engineers"
-  account_identifiers           = var.account_id
-  attributes                    = local.attributes
-  actions_allowed_no_tags       = local.actions_allowed_no_tags
-  actions_allowed_matching_tags = local.actions_allowed_matching_tags
+module "ec2_abac_tempest" {
+  source                         = "../.."
+  policy_name                    = "AllowEC2Engineers"
+  account_identifiers            = ["123456789012", "210987654321"] # Replace with your own AWS Account IDs
+  attributes                     = local.attributes
+  actions_allowed_nonconditional = local.actions_allowed_nonconditional
+  actions_allowed_matching_tags  = local.actions_allowed_matching_tags
 }
