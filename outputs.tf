17f743d576b4b6e09eafd0d172c27f7083d9f3a8
HK Transfield <hktransfield.dev@gmail.com>
2024-08-22 13:44:30 +1200

    feat: output permission_set_arn and permission_set_created_date

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "arn" {
+output "group_id" {
+  description = "The group ID that has the custom permission set attached to it"
+  value       = data.aws_identitystore_group.this.group_id
+}
+
+output "permission_set_arn" {
   description = "The Amazon Resource Number (ARN) of the custom permission set"
   value       = aws_ssoadmin_permission_set.this.arn
 }
 
-output "group_id" {
-  description = "The group ID that has the custom permission set attached to it"
-  value       = data.aws_identitystore_group.this.group_id
+output "permission_set_created_date" {
+  description = "The date the Permission Set was created in RFC3339 format"
+  value       = data.aws_ssoadmin_permission_set.this.created_date
 }
