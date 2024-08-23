73474eb335fa53a7921a25c64b67bd052776ce4c
HK Transfield <hktransfield.dev@gmail.com>
2024-08-23 12:03:52 +1200

    chore: change group_id to principal_id

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "group_id" {
-  description = "The group ID that has the custom permission set attached to it"
-  value       = data.aws_identitystore_group.this.group_id
+output "principal_id" {
+  description = "The principal ID that has the custom permission set attached to it"
+  value       = local.principal_id
 }
 
 output "permission_set_arn" {
@@ -11,4 +11,9 @@ output "permission_set_arn" {
 output "permission_set_created_date" {
   description = "The date the Permission Set was created in RFC3339 format"
   value       = data.aws_ssoadmin_permission_set.this.created_date
+}
+
+output "account_assignment" {
+  description = "All account assignments made"
+  value       = aws_ssoadmin_account_assignment.this
 }
