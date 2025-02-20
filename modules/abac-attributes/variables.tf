1f0500dfe14fe3f2b62facae3380d33484bd5472
HK Transfield <hktransfield.dev@gmail.com>
2025-02-20 14:22:52 +1300

    feat: add validation and default behaviour

--- modules/abac-attributes/variables.tf
+++ modules/abac-attributes/variables.tf
@@ -3,4 +3,21 @@ variable "attributes" {
   type        = map(string)
   default     = {}
   nullable    = false
+
+  validation {
+    condition     = alltrue([for k, v in var.attributes : can(regex("^[a-zA-Z0-9_-]+$", k)) && can(regex("^[a-zA-Z0-9_/.-]+$", v))])
+    error_message = "Keys and values must be alphanumeric with underscores, hyphens, or forward slashes"
+  }
+}
+
+variable "default_behavior" {
+  description = "The default behavior of the attribute-based access control configuration. This value is used when the attribute value is not found in the policy."
+  type        = string
+  default     = "DENY"
+
+  validation {
+    condition     = can(regex("^(ALLOW|DENY)$", var.default_behavior))
+    error_message = "Default behavior must be either ALLOW or DENY"
+  }
+
 }
